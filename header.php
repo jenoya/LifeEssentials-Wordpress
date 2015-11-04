5a4e816e606fac2b39829733a8fe47fe8e36f297
jenoya <jennifer.oya.101@my.csun.edu>
2015-11-04 13:38:45 -0800

    fixed category sort
    fixed sort categories
    tried to add bootstrap tabs
    failing to successfully add tabs

--- header.php
+++ header.php
@@ -4,15 +4,19 @@
 	<title><?php if(is_home()) bloginfo('name'); else bloginfo('name'); wp_title(''); ?></title>
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
-	<!-- masonry css -->
+<!-- masonry css -->
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/masonry.css">
+<!-- categories.css -->
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/categories.css">
+<!-- JAVA JQUERY -->
 	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
 	<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<!-- BOOTSTRAP JQUERY -->
 	<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/bootstrap.min.js"></script>
-	<!-- masonry -->
+<!-- masonry -->
 	<script src="<?php bloginfo('template_directory'); ?>/js/masonry.pkgd.js"></script>
-	
-	<!-- font -->
+
+<!-- font -->
 	<link href='https://fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
 	
 	<?php wp_head();?>
--- page-posts.php
+++ page-posts.php
@@ -4,34 +4,42 @@
 	<div class="col-xs-12 col-sm-9 content">
 		
 		<h1><?php the_title();?></h1>
-		
-		<?php $edcinterviews_posts = new WP_Query( array( 'post_type' => 'post', 'category_name' => 'edc-interviews'); ?>
 
+		<ul class="nav nav-tabs nav-justified category-nav">
+			<li role="presentation" class="active">
+				<a href="#">Edc Interviews</a>
+				
+				<?php query_posts('category_name=edc-interviews'); ?>
+					<ul>
+						<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+						<li>
+							<h2 class="recent-posts">
+								<a href="<?php the_permalink() ?>">
+									<?php the_title(); ?>
+								</a>
+							</h2>
+							<?php
+								if ( has_post_thumbnail() ) {
+									the_post_thumbnail();
+								}
+								else {
+									echo '<img src="' . get_bloginfo( 'stylesheet_directory' ) . '/images/thumbnail-default.jpg" class="img-responsive" />';
+								}
+								?>
+						</li>
+						<?php endwhile; ?>
+					</ul>
+				<?php endif; ?>
 
-		
-
-		<?php if($edcinterviews_posts = have_posts()): while ($edcinterviews_posts = have_posts()) : $edcinterviews_posts = the_post(); ?>
-			<?php
-			// Must be inside a loop.
-
-				if ( has_post_thumbnail() ) {
-					the_post_thumbnail();
-				}
-				else {
-					echo '<img src="' . get_bloginfo( 'stylesheet_directory' ) . '/images/thumbnail-default.jpg" class="img-responsive" />';
-				}
-			?>
+			</li>
+			<li role="presentation"><a href="#">Product Reviews</a></li>
+			<li role="presentation"><a href="#">Uncategorized</a></li>
 			
-			<h2 class="recent-posts">
-				<a href="<?php the_permalink() ?>">
-					<?php the_title(); ?>
-				</a>
-			</h2>
-			<p><?php the_excerpt(); ?><a href="<?php echo get_permalink(); ?>"> Read More...</a></p>
+		</ul>
 
-		<?php endwhile; endif; ?>
+				
 
-
+		
 
 	</div>
 
